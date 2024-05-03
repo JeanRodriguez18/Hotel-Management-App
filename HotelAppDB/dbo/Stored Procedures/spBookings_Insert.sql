14e2245822d963fb3dc13f43fe1c3925c30432eb
Jean Carlos Rodriguez Tavarez <jeanrodriguez@presidencia.gob.do>
2024-05-03 17:12:23 -0400

    Adding store procedure to get available rooms Adding store procudure to insert guests in case guest exists it returns the guest Adding store procedure to insert bookings Adding method to allow guests to book rooms Adding models for guest and rooms

---
+++ HotelAppDB/dbo/Stored Procedures/spBookings_Insert.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[spBookings_Insert]
+	@roomId int,
+	@guestId int,
+	@startDate date,
+	@endDate date,
+	@totalPrice money
+
+AS
+begin
+	set nocount on;
+
+	insert into dbo.Bookings(RoomId, GuestId, StartDate, EndDate, TotalPrice)
+	values(@roomId, @guestId, @startDate, @endDate, @totalPrice);
+end
--- HotelAppLibrary/Data/SqlData.cs
+++ HotelAppLibrary/Data/SqlData.cs
@@ -2,6 +2,8 @@
 using HotelAppLibrary.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace HotelAppLibrary.Data
@@ -21,6 +23,22 @@ namespace HotelAppLibrary.Data
             return _db.LoadData<RoomTypeModel, dynamic>("dbo.spRoomTypes_GetAvailableTypes", new {startDate, endDate}, connectionStringName, true);
         }
 
+
+        public void BookGuests(string firstName, string lastName, DateTime startDate, DateTime endDate, int roomTypeId)
+        {
+
+            GuestModel guest = _db.LoadData<GuestModel, dynamic>("dbo.spGuests_Insert", new { firstName, lastName }, connectionStringName, true).First();
+
+
+            RoomTypeModel roomtype = _db.LoadData<RoomTypeModel, dynamic>("select * from dbo.roomTypes where Id = @Id", new { Id = roomTypeId }, connectionStringName, false).First();
+
+            TimeSpan timeStaying = endDate.Date.Subtract(startDate.Date);
+
+            List<RoomModel> availableRooms = _db.LoadData<RoomModel, dynamic>("dbo.spRoom_GetAvailableRooms", new {startDate, endDate, roomTypeId }, connectionStringName, true);
+
+            _db.SaveData("dbo.spBookings_Insert", new { roomId = availableRooms.First().Id, guestId = guest.Id, startDate = startDate, endDate = endDate, totalPrice = timeStaying.Days * roomtype.Price }, connectionStringName, true);
+
+        }
 
     }
 }
