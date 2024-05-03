af10ee2dc17c5706a2f055139c44ecee23303800
Jean Carlos Rodriguez Tavarez <jeanrodriguez@presidencia.gob.do>
2024-05-03 14:55:01 -0400

    Implementing Sql data access with dapper and sql data access interface Adding Room type model Adding  store procedure to get available rooms

---
+++ HotelAppLibrary/Databases/ISqlDataAccess.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace HotelAppLibrary.Databases
+{
+    public interface ISqlDataAccess
+    {
+        List<T> LoadData<T, U>(string sqlStatement, U paramemters, string connectionStringName, bool isStoreProcedure = false);
+        void SaveData<T>(string sqlStatement, T parameters, string connectionStringName, bool isStoreProcedure = false);
+    }
+}
---
+++ HotelAppLibrary/Databases/SqlDataAccess.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+namespace HotelAppLibrary.Databases
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _config;
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+
+        public List<T> LoadData<T, U>(string sqlStatement, U paramemters, string connectionStringName, bool isStoreProcedure = false)
+        {
+            string connectionString = _config.GetConnectionString(connectionStringName);
+
+            //This is the default for normal querys (select and so on)
+            CommandType commandType = CommandType.Text;
+
+            if (isStoreProcedure == true)
+            {
+                commandType = CommandType.StoredProcedure;
+            }
+
+            using (IDbConnection cnn = new SqlConnection(connectionString))
+            {
+                List<T> rows = cnn.Query<T>(sqlStatement, paramemters, commandType: commandType).ToList();
+                return rows;
+            }
+        }
+
+
+        public void SaveData<T>(string sqlStatement, T parameters, string connectionStringName, bool isStoreProcedure = false)
+        {
+            string connectionString = _config.GetConnectionString(connectionStringName);
+
+            //This is the default for normal querys (select and so on)
+            CommandType commandType = CommandType.Text;
+
+            if (isStoreProcedure == true)
+            {
+                commandType = CommandType.StoredProcedure;
+            }
+
+            using (IDbConnection cnn = new SqlConnection())
+            {
+                cnn.Execute(sqlStatement, parameters, commandType: commandType);
+            }
+        }
+
+
+    }
+}
