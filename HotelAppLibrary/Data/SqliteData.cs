a2f50b96d2d722b0baefaaa4ddcf20aaea24dce8
Jean Rodriguez <jeancarlosrt301@gmail.com>
2024-05-08 23:36:23 -0400

    Adding get available room types method in sqlite

--- HotelAppLibrary/Data/SqliteData.cs
+++ HotelAppLibrary/Data/SqliteData.cs
@@ -2,15 +2,16 @@
 using HotelAppLibrary.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace HotelAppLibrary.Data
 {
     public class SqliteData : IDatabaseData
     {
-        private readonly ISqlDataAccess _db;
+        private readonly ISqliteDataAccess _db;
         private readonly string connectionStringName = "SqliteDb";
-        public SqliteData(ISqlDataAccess db)
+        public SqliteData(ISqliteDataAccess db)
         {
             _db = db;
         }
@@ -27,7 +28,24 @@ namespace HotelAppLibrary.Data
 
         public List<RoomTypeModel> GetAvailablesRoomTypes(DateTime startDate, DateTime endDate)
         {
-            throw new NotImplementedException();
+            string sql = @"select rt.Id, rt.Title, rt.Description, rt.Price
+	                    from Rooms r
+	                    inner join RoomTypes rt on rt.id = r.RoomTypeId
+	                    where r.Id not in (
+	                    select b.RoomId
+	                    from Bookings b 
+	                    where (@startDate < b.StartDate and @endDate > b.EndDate)
+		                    or (b.StartDate <= @endDate and @endDate < b.EndDate)
+		                    or (b.StartDate <= @startDate and @startDate < b.EndDate)
+	                    )
+	                    group by rt.Id, rt.Title, rt.Description, rt.Price";
+
+            var output = _db.LoadData<RoomTypeModel, dynamic>(sql, new { startDate, endDate }, connectionStringName).ToList();
+
+
+            output.ForEach(x => x.Price = x.Price / 100);
+
+            return output;
         }
 
         public RoomTypeModel GetRoomTypeById(int id)
